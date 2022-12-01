1fb83aa7bfa838e69bf4aed12fe2f566038413c7
Walter Huf <hufman@gmail.com>
2022-12-01 08:51:19 -0800

    Day1a

---
+++ src/day1.rs
@@ -0,0 +1,22 @@
+extern crate itertools;
+
+use itertools::Itertools;
+
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<Vec<usize>> {
+    input.lines()
+        .map(|line| line.parse::<usize>())
+        .group_by(|elt| elt.is_ok())
+        .into_iter()
+        .filter_map(|(key, results)| if key {Some(results)} else {None})
+        .map(|results| results.into_iter().map(|r| r.unwrap()).collect())
+        .collect()
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(elves: &[Vec<usize>]) -> usize {
+    elves.iter()
+        .map(|elf| elf.iter().sum())
+        .max()
+        .unwrap()
+}
