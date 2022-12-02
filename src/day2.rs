50392dd9335a294841ef1fe3512ee476a4626f93
Walter Huf <hufman@gmail.com>
2022-12-01 22:23:48 -0800

    Day2b

--- src/day2.rs
+++ src/day2.rs
@@ -1,8 +1,9 @@
 use std::cmp::Ordering;
 use std::cmp::Ordering::{Equal, Greater, Less};
 use crate::day2::Move::{PAPER, ROCK, SCISSORS};
+use crate::day2::Outcome::{DRAW, LOSE, WIN};
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Copy, Clone)]
 pub enum Move {
     ROCK,
     PAPER,
@@ -30,20 +31,37 @@ impl PartialOrd for Move {
         else { Some(Greater) }
     }
 }
+#[derive(PartialEq)]
+pub enum Outcome {
+    LOSE,
+    DRAW,
+    WIN,
+}
 
-fn parse(line: &str) -> (Move, Move) {
+impl From<&str> for Outcome {
+    fn from(input: &str) -> Self {
+        match input {
+            "X" => LOSE,
+            "Y" => DRAW,
+            "Z" => WIN,
+            _ => panic!("Unknown input {}", input)
+        }
+    }
+}
+
+fn parse(line: &str) -> (Move, Move, Outcome) {
     let pieces = line.split_at(1);
-    (Move::from(pieces.0.trim()), Move::from(pieces.1.trim()))
+    (Move::from(pieces.0.trim()), Move::from(pieces.1.trim()), Outcome::from(pieces.1.trim()))
 }
 
 #[aoc_generator(day2)]
-pub fn input_generator(input: &str) -> Vec<(Move, Move)> {
+pub fn input_generator(input: &str) -> Vec<(Move, Move, Outcome)> {
     input.lines()
         .map(|line| parse(line))
         .collect()
 }
 
-fn score_part1(left: &Move, right: &Move) -> usize {
+fn score_round(left: &Move, right: &Move) -> usize {
     let score = match left.partial_cmp(&right) {
         Some(Less) => 6,
         Some(Equal) => 3,
@@ -59,8 +77,34 @@ fn score_part1(left: &Move, right: &Move) -> usize {
 }
 
 #[aoc(day2, part1)]
-pub fn solve_part1(plays: &[(Move, Move)]) -> usize {
+pub fn solve_part1(plays: &[(Move, Move, Outcome)]) -> usize {
     plays.iter()
-        .map(|(l, r)| score_part1(l, r))
+        .map(|(l, r, _)| score_round(l, r))
+        .sum()
+}
+
+fn calculate_round(left: &Move, outcome: &Outcome) -> Move {
+    if outcome == &Outcome::DRAW { left.clone() }
+    else if outcome == &Outcome::WIN {
+        match left {
+            ROCK => PAPER,
+            PAPER => SCISSORS,
+            SCISSORS => ROCK,
+        }
+    }
+    else {
+        match left {
+            ROCK => SCISSORS,
+            PAPER => ROCK,
+            SCISSORS => PAPER,
+        }
+    }
+}
+
+#[aoc(day2, part2)]
+pub fn solve_part2(plays: &[(Move, Move, Outcome)]) -> usize {
+    plays.iter()
+        .map(|(l, _, o)| (l, calculate_round(l, o)))
+        .map(|(l, r)| score_round(l, &r))
         .sum()
 }
